330e009f7ceef3c91c2c8f9fb9ecbf1afd81d091
Anastasia <samya.khemri@ubisoft.com>
2019-05-08 16:02:31 -0400

    init of the app

---
+++ src/article/components/Article.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <h1> ARTICLE {{ article.title }} </h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Article",
+  data() {
+    return {
+      article: {}
+    }
+  },
+
+  mounted() {
+    fetch(`http://localhost:3000/articles/${this.$route.params.id}`)
+      .then(response => {
+        if (response.ok) {
+          return response.json()
+        }
+      })
+      .then(article => (this.article = article))
+  }
+}
+</script>
+
---
+++ src/listArticle/components/ListArticles.vue
@@ -0,0 +1,40 @@
+<template>
+  <v-container fluid grid-list>
+    <v-layout row wrap>
+      <v-card v-for="article in articles" :key="article._id">
+        <v-card-title primary-title>
+          <div>
+            <div class="headline">{{ article.title }}</div>
+            <span> {{article.content}} </span>
+          </div>
+        </v-card-title>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn> Read More </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "ListArticles",
+
+  data() {
+    return {
+      articles: []
+    }
+  },
+
+  mounted() {
+    fetch("http://localhost:3000/articles")
+      .then(response => {
+        if (response.ok) {
+          return response.json()
+        }
+      })
+      .then(articles => (this.articles = articles))
+  }
+}
+</script>
